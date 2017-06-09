17a88dc6f4e6feccf02596248d75af4e58b9fa9a
Joshua David Clayton <joshdavidclayton@gmail.com>
2017-06-09 12:28:44 -0400

    Babelify preset
    * Add build script
    * Use env preset
    *

---
+++ src/excludes.js
@@ -0,0 +1,32 @@
+export const defaultExcludes = [
+  'es6.reflect.apply',
+  'es6.reflect.construct',
+  'es6.reflect.define-property',
+  'es6.reflect.delete-property',
+  'es6.reflect.get',
+  'es6.reflect.get-own-property-descriptor',
+  'es6.reflect.get-prototype-of',
+  'es6.reflect.has',
+  'es6.reflect.is-extensible',
+  'es6.reflect.own-keys',
+  'es6.reflect.prevent-extensions',
+  'es6.reflect.set',
+  'es6.reflect.set-prototype-of',
+  'es6.typed.data-view',
+  'es6.typed.int8-array',
+  'es6.typed.uint8-array',
+  'es6.typed.uint8-clamped-array',
+  'es6.typed.int16-array',
+  'es6.typed.uint16-array',
+  'es6.typed.int32-array',
+  'es6.typed.uint32-array',
+  'es6.typed.float32-array',
+  'es6.typed.float64-array'
+];
+
+export const defaultNodeExcludes = [
+  'transform-regenerator',
+  'web.timers',
+  'web.immediate',
+  'web.dom.iterable'
+];
