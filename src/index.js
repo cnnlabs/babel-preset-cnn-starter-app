571a5ab4d1d9211e0b7cde367157110ca4d4bb51
Joshua David Clayton <joshdavidclayton@gmail.com>
2017-06-12 12:22:15 -0400

    Clean up preset, add server env to include css modules

--- src/index.js
+++ src/index.js
@@ -39,6 +39,15 @@ const config = {
         require.resolve('react-hot-loader/babel')
     ],
     env: {
+        server: {
+            plugins: [
+                require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
+                [require.resolve('babel-plugin-css-modules-transform'), {
+                    generateScopedName: '[hash:8]',
+                    extensions: ['.css']
+                }]
+            ]
+        },
         test: {
             plugins: [
                 require.resolve('babel-plugin-transform-es2015-modules-commonjs')
@@ -48,41 +57,3 @@ const config = {
 }
 
 export default config;
-
-// const config = {
-//     'env': {
-//         'server': {
-//             'plugins': [
-//                 require.resolve('babel-plugin-transform-es2015-modules-commonjs'),
-//                 [ require.resolve('babel-plugin-css-modules-transform'), {
-//                     'generateScopedName': '[hash:8]',
-//                     'extensions': [ '.css' ]
-//                 } ]
-//             ]
-//         }
-//     }
-// };
-
-// if (process.env.NODE_ENV === 'test') {
-//     module.exports = Object.assign({}, config, {
-//         presets: [
-//             require.resolve('babel-preset-es2015'),
-//             require.resolve('babel-preset-stage-2'),
-//             require.resolve('babel-preset-flow'),
-//             require.resolve('babel-preset-syntax-jsx'),
-//             require.resolve('babel-plugin-transform-react-jsx'),
-//             require.resolve('babel-plugin-transform-react-display-name')
-//         ]
-//     })
-// } else {
-//     module.exports = Object.assign({}, config, {
-//         presets: [
-//             [ require.resolve('babel-preset-es2015'), { modules: false } ],
-//             require.resolve('babel-preset-stage-2'),
-//             require.resolve('babel-preset-react')
-//         ],
-//         plugins: [
-//             require.resolve('react-hot-loader/babel')
-//         ]
-//     });
-// }
